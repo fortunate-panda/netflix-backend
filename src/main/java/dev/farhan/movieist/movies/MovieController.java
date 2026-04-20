27fce43fc85c615a15e8101deff0035af07a4eaa
Fortunate-Panda <0xfortunatepanda@proton.me>
2026-04-20 13:13:57 +0100

    Update MovieController.java

--- src/main/java/dev/farhan/movieist/movies/MovieController.java
+++ src/main/java/dev/farhan/movieist/movies/MovieController.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController { ... }
+
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
